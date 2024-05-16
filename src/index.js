8d09420471e48066e0dbabf595496646bf410e70
Eiker <eikercifuentes7@gmail.com>
2024-05-16 09:12:14 -0500

    borrado comentarios

--- src/index.js
+++ src/index.js
@@ -6,12 +6,7 @@ let app = express()
 app.get('/', function (req, res) {
   res.send( dice.roll())
 })
-// process.on('SIGTERM', function() {
-//   console.log('Received event SIGTERM')
-//   console.log('Saving state and doing important stuff')
-//   process.exit(0);
-// });
-//Launch listening server on port 8080 and consoles the log.
+
 app.listen(8080, function () {
   console.log('app listening on port 8080!')
 })
